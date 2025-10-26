e5ae7df6c97a3bbffad78c2a8d5dfb7eb1312d29
Reda-Shalabi <redavd12@gmail.com>
2025-10-26 23:00:55 +0300

    update portfolio

---
+++ src/views/HomeView.vue
@@ -0,0 +1,273 @@
+<template>
+  <main
+    class="mt-10 md:mt-1 flex flex-col-reverse gap-8 items-center md:flex-row md:gap-16 md:justify-center min-h-[65vh] md:min-h-[80vh]"
+  >
+    <div class="space-y-2 text-center md:text-left px-6 md:px-10">
+      <p class="text-amber-200 text-sm md:text-base">Hello World, I'm</p>
+      <h1 class="text-4xl md:text-5xl font-bold text-white fadein-up">
+        Reda Shalabi
+      </h1>
+      <div class="py-2">
+        <h1
+          class="typewrite text-lg md:text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-slate-100 to-yellow-500 fadein-up"
+          ref="typewriter"
+        >
+          <span class="wrap">{{ txt }}</span>
+        </h1>
+      </div>
+      <p class="text-white fade-in-from-left leading-relaxed">
+        Welcome to my personal website. <span class="wave">👋🏼</span>
+      </p>
+
+      <div class="pt-5 flex justify-center md:justify-start">
+        <a
+          href="/pdf/cv.pdf"
+          download="Reda-Shalabi-CV.pdf"
+          class="fadein-bot fade-500 flex items-center py-2 px-4 text-sm md:text-base font-medium rounded-lg border border-amber-200 text-amber-200 transition duration-300 hover:bg-amber-200 hover:bg-opacity-10 bg-transparent focus:outline-none w-full sm:w-fit"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            class="mr-2 w-4 h-4 md:w-5 md:h-5"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M5.625 1.5H9a3.75 3.75 0 013.75 3.75v1.875c0 1.036.84 1.875 1.875 1.875H16.5a3.75 3.75 0 013.75 3.75v7.875c0 1.035-.84 1.875-1.875 1.875H5.625a1.875 1.875 0 01-1.875-1.875V3.375c0-1.036.84-1.875 1.875-1.875zm5.845 17.03a.75.75 0 001.06 0l3-3a.75.75 0 10-1.06-1.06l-1.72 1.72V12a.75.75 0 00-1.5 0v4.19l-1.72-1.72a.75.75 0 00-1.06 1.06l3 3z"
+              clip-rule="evenodd"
+            ></path>
+            <path
+              d="M14.25 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0016.5 7.5h-1.875a.375.375 0 01-.375-.375V5.25z"
+            ></path>
+          </svg>
+          Download Resume
+        </a>
+      </div>
+      </div>
+
+<div class="flex fadein-right">
+ <img
+  alt="avatar"
+  class="ml-auto w-[200px] h-[200px] sm:w-[250px] sm:h-[250px] md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[400px] transition-all duration-500 hover:scale-105 rounded-full border-4 border-amber-200 pict float"
+  src="https://i.postimg.cc/1RWtjZrw/photo-2025-10-26-12-49-27.jpg"
+/>
+
+</div>
+
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      toRotate: ["Web Developer", "Full-stack Engineer", "Computer Science Student", "Tech Enthusiast"],
+      period: 2000,
+      txt: '',
+      loopNum: 0,
+      isDeleting: false,
+    };
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.tick();
+    });
+  },
+  methods: {
+    tick() {
+      let typewriter = this.$refs.typewriter;
+
+      if (!typewriter) {
+        return;
+      }
+
+      
+
+      let i = this.loopNum % this.toRotate.length;
+      let fullTxt = this.toRotate[i];
+
+      this.txt = this.isDeleting ? fullTxt.substring(0, this.txt.length - 1) : fullTxt.substring(0, this.txt.length + 1);
+      typewriter.innerHTML = `<span class="wrap">${this.txt}</span>`;
+
+      let that = this;
+      let delta = 200 - Math.random() * 100;
+
+      if (this.isDeleting) {
+        delta /= 2;
+      }
+
+      if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+      } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+      }
+
+      setTimeout(() => {
+        that.tick();
+      }, delta);
+    },
+  }
+}
+</script>
+
+<style>
+body {
+  overflow-y: scroll;
+  overflow-x: hidden;
+}
+
+
+.typewrite>.wrap {
+  border-right: 0.08em solid #fff;
+}
+
+.wave {
+  animation-name: wave-animation;
+  animation-duration: 2.5s;
+  animation-iteration-count: infinite;
+  transform-origin: 70% 70%;
+  display: inline-block
+}
+
+@keyframes wave-animation {
+  0% {
+    transform: rotate(0deg)
+  }
+
+  10% {
+    transform: rotate(14deg)
+  }
+
+  20% {
+    transform: rotate(-8deg)
+  }
+
+  30% {
+    transform: rotate(14deg)
+  }
+
+  40% {
+    transform: rotate(-4deg)
+  }
+
+  50% {
+    transform: rotate(10deg)
+  }
+
+  60% {
+    transform: rotate(0deg)
+  }
+
+  to {
+    transform: rotate(0deg)
+  }
+}
+
+.pict {
+  box-shadow: 0px 0px 73px -9px rgba(255,219,112,0.44);
+-webkit-box-shadow: 0px 0px 73px -9px rgba(255,219,112,0.44);
+-moz-box-shadow: 0px 0px 73px -9px rgba(255,219,112,0.44);
+}
+
+.fadein-up {
+  opacity: 0;
+  animation-name: fadeInUp;
+  animation-duration: 0.5s;
+  animation-fill-mode: forwards;
+  animation-delay: 500ms;
+}
+
+@keyframes fadeInUp {
+  from {
+    opacity: 0;
+    transform: translate3d(0, 100%, 0);
+  }
+
+  to {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
+}
+
+.fade-in-from-left {
+  opacity: 0;
+  animation: fadeInLeft 0.5s ease-out forwards;
+  animation-delay: 500ms;
+}
+
+@keyframes fadeInLeft {
+  0% {
+    opacity: 0;
+    transform: translateX(-100%);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+
+.fadein-right {
+  opacity: 0;
+  animation: fadeInRight 0.5s ease-out forwards;
+  animation-delay: 500ms;
+}
+
+@keyframes fadeInRight {
+  0% {
+    opacity: 0;
+    transform: translateX(100%);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+
+.fadein-bot {
+  opacity: 0;
+  animation: fadeInBot 0.5s forwards;
+}
+
+@keyframes fadeInBot {
+  from {
+    opacity: 0;
+    transform: translate3d(0, -100%, 0);
+  }
+
+  to {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
+}
+
+.fadein-1 {
+  animation-delay: 200ms;
+}
+.fadein-2 {
+  animation-delay: 400ms;
+}
+.fadein-3 {
+  animation-delay: 600ms;
+}
+.fade-500 {
+  animation-delay: 500ms;
+}
+
+@keyframes float {
+  0%, 100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-20px);
+  }
+}
+
+.float {
+  animation: float 3s ease-in-out infinite;
+}
+
+</style>
